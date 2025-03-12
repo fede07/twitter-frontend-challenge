86f69ad74f68fedd86ea5b21390eef542c674484
Federico Rossendy <fede070893@gmail.com>
2025-03-12 12:15:05 -0300

    Convert PostPage class component to functional component
    Refactored PostPage to use React functional components and hooks, replacing the class-based implementation. Introduced `useEffect` for lifecycle management and replaced state handling with `useState`. Removed HttpService in favor of custom hooks for fetching user and post data.

--- src/pages/post-page/PostPage.tsx
+++ src/pages/post-page/PostPage.tsx
@@ -1,92 +1,170 @@
-import React, { Component } from 'react';
-import { StyledContainer } from '../../components/common/Container';
-import Tweet from '../../components/tweet/Tweet';
+import { useEffect, useState } from 'react';
+import { Post } from '../../service';
+import { UseGetProfile } from '../../queries/userQueries';
+import { UseGetPostById } from '../../queries/postQueries';
 import Loader from '../../components/loader/Loader';
-import { HttpService } from '../../service/HttpRequestService';
-import TweetBox from '../../components/tweet-box/TweetBox';
+import { StyledContainer } from '../../components/common/Container';
 import { StyledH5 } from '../../components/common/text';
 import { StyledFeedContainer } from '../home-page/components/contentContainer/FeedContainer';
+import Tweet from '../../components/tweet/Tweet';
+import TweetBox from '../../components/tweet-box/TweetBox';
 import CommentFeed from '../../components/feed/CommentFeed';
-import { Post } from '../../service';
+import { useToast } from '../../context/ToastContext';
+import { ToastType } from '../../components/toast/Toast';
 
-interface PostPageProps {}
+// interface PostPageProps {}
+//
+// interface PostPageState {
+//   postId: string;
+//   post: Post | undefined;
+// }
 
-interface PostPageState {
-  postId: string;
-  post: Post | undefined;
-}
+const PostPage = () => {
+  const [postId, setPostId] = useState('');
+  const [post, setPost] = useState<Post | undefined>(undefined);
+  const { showToast } = useToast();
 
-class PostPage extends Component<PostPageProps, PostPageState> {
-  private service: HttpService['service'];
+  const {
+    data: user,
+    isLoading: isLoadingUser,
+    error: errorUser,
+    isError: isErrorUser,
+  } = UseGetProfile();
 
-  constructor(props: PostPageProps) {
-    super(props);
+  const {
+    data: post_,
+    isLoading: isLoadingPost,
+    error: errorPost,
+    isError: isErrorPost,
+  } = UseGetPostById(postId);
 
-    this.state = {
-      postId: window.location.href.split('/')[4] || '',
-      post: undefined,
-    };
+  const fetchPost = () => {
+    if (!post_ || !user) return;
+    setPost(post_);
+    setPostId(post_.id);
+  };
 
-    this.service = new HttpService().service;
+  useEffect(() => {
+    fetchPost();
+  }, [postId]);
+
+  if (isLoadingUser || isLoadingPost) return <Loader />;
+
+  if (isErrorUser ) {
+    showToast(errorUser.message, ToastType.ALERT);
+    return <Loader />;
   }
 
-  componentDidMount() {
-    this.fetchPost();
+  if (isErrorPost) {
+    showToast(errorPost.message, ToastType.ALERT);
+    return <Loader />;
   }
 
-  componentDidUpdate(prevProps: PostPageState, prevState: PostPageState) {
-    if (prevState.postId !== this.state.postId) {
-      this.fetchPost();
-    }
-  }
-
-  fetchPost() {
-    this.service
-      .getPostById(this.state.postId)
-      .then((res: Post) => {
-        this.setState({ post: res });
-      })
-      .catch((e: unknown) => {
-        console.log(e);
-      });
-
-    console.log(this.state.post)
-  }
-
-  render() {
-    return (
-      <StyledContainer borderRight={'1px solid #ebeef0'}>
-        <StyledContainer
-          padding={'16px'}
-          borderBottom={'1px solid #ebeef0'}
-          maxHeight={'53px'}
-        >
-          <StyledH5>Tweet</StyledH5>
-        </StyledContainer>
-        <StyledFeedContainer>
-          {this.state.post ? (
-            <>
-              <Tweet post={this.state.post} />
-              <StyledContainer
-                borderBottom={'1px solid #ebeef0'}
-                padding={'16px'}
-              >
-                <TweetBox parentId={this.state.postId} />
-              </StyledContainer>
-
-              <StyledContainer minHeight={'53.5vh'}>
-                <CommentFeed postId={this.state.postId} />
-              </StyledContainer>
-            </>
-          ) : (
-            <StyledContainer justifyContent={'center'} alignItems={'center'}>
-              <Loader />
-            </StyledContainer>
-          )}
-        </StyledFeedContainer>
+  return (
+    <StyledContainer borderRight={'1px solid #ebeef0'}>
+      <StyledContainer
+        padding={'16px'}
+        borderBottom={'1px solid #ebeef0'}
+        maxHeight={'53px'}
+      >
+        <StyledH5>Tweet</StyledH5>
       </StyledContainer>
-    );
-  }
-}
+      <StyledFeedContainer>
+        {post ? (
+          <>
+            <Tweet post={post} />
+            <StyledContainer
+              borderBottom={'1px solid #ebeef0'}
+              padding={'16px'}
+            >
+              <TweetBox parentId={postId} />
+            </StyledContainer>
+
+            <StyledContainer minHeight={'53.5vh'}>
+              <CommentFeed postId={postId} />
+            </StyledContainer>
+          </>
+        ) : (
+          <StyledContainer justifyContent={'center'} alignItems={'center'}>
+            <Loader />
+          </StyledContainer>
+        )}
+      </StyledFeedContainer>
+    </StyledContainer>
+  );
+};
+
+// class PostPage extends Component<PostPageProps, PostPageState> {
+//   private service: HttpService['service'];
+//
+//   constructor(props: PostPageProps) {
+//     super(props);
+//
+//     this.state = {
+//       postId: window.location.href.split('/')[4] || '',
+//       post: undefined,
+//     };
+//
+//     this.service = new HttpService().service;
+//   }
+//
+//   componentDidMount() {
+//     this.fetchPost();
+//   }
+//
+//   componentDidUpdate(prevProps: PostPageState, prevState: PostPageState) {
+//     if (prevState.postId !== this.state.postId) {
+//       this.fetchPost();
+//     }
+//   }
+//
+//   fetchPost() {
+//     this.service
+//       .getPostById(this.state.postId)
+//       .then((res: Post) => {
+//         this.setState({ post: res });
+//       })
+//       .catch((e: unknown) => {
+//         console.log(e);
+//       });
+//
+//     console.log(this.state.post)
+//   }
+//
+//   render() {
+//     return (
+//       <StyledContainer borderRight={'1px solid #ebeef0'}>
+//         <StyledContainer
+//           padding={'16px'}
+//           borderBottom={'1px solid #ebeef0'}
+//           maxHeight={'53px'}
+//         >
+//           <StyledH5>Tweet</StyledH5>
+//         </StyledContainer>
+//         <StyledFeedContainer>
+//           {this.state.post ? (
+//             <>
+//               <Tweet post={this.state.post} />
+//               <StyledContainer
+//                 borderBottom={'1px solid #ebeef0'}
+//                 padding={'16px'}
+//               >
+//                 <TweetBox parentId={this.state.postId} />
+//               </StyledContainer>
+//
+//               <StyledContainer minHeight={'53.5vh'}>
+//                 <CommentFeed postId={this.state.postId} />
+//               </StyledContainer>
+//             </>
+//           ) : (
+//             <StyledContainer justifyContent={'center'} alignItems={'center'}>
+//               <Loader />
+//             </StyledContainer>
+//           )}
+//         </StyledFeedContainer>
+//       </StyledContainer>
+//     );
+//   }
+// }
 
 export default PostPage;
